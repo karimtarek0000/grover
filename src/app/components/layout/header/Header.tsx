ce27fff042fa4759eb4e541f303e7392a99a7286
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-07-02 18:41:36 +0300

    header | images

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
-import img1 from "@/assets/images/section-1/1.svg";
+import img1 from "@/assets/images/section-1/1.png";
+import headerImg1 from "@/assets/images/section-1/header/1.png";
+import headerImg2 from "@/assets/images/section-1/header/2.png";
+import headerImg3 from "@/assets/images/section-1/header/3.png";
+import headerImg4 from "@/assets/images/section-1/header/4.png";
 import Style from "./style.module.css";
 import StyleButton from "../../shared/buttons/style.module.css";
 import RenderSVG from "../../shared/SVG/RenderSVG";
@@ -13,8 +17,8 @@ const Header = (): JSX.Element => {
       <div className="md:basis-1/2">
         <div className={note}>
           More than Faster
-          <div className="w-[3.9375rem] h-[3.125rem]">
-            <Image src={img1} alt="img" className=" h-[4.25rem]" />
+          <div className="w-[3.9375rem] h-[3.125rem] pt-1">
+            <Image src={img1} alt="img" width="100" />
           </div>
         </div>
 
@@ -38,7 +42,33 @@ const Header = (): JSX.Element => {
           </Button>
         </div>
       </div>
-      <div className="md:basis-1/2">2</div>
+
+      <div className="relative flex items-end justify-center md:basis-1/2">
+        <Image
+          src={headerImg1}
+          alt="header-one"
+          width="182"
+          height="272"
+          className="absolute start-0 top-[25%] z-10"
+        />
+        <div className="w-[21.875rem] h-fit overflow-y-hidden flex-none">
+          <Image src={headerImg2} alt="header-two" className="-mt-12" />
+        </div>
+        <Image
+          src={headerImg3}
+          alt="header-three"
+          width="280"
+          height="74"
+          className="absolute top-[20%] end-0"
+        />
+        <Image
+          src={headerImg4}
+          alt="header-foure"
+          width="120"
+          height="156"
+          className="absolute top-[40%] end-14 -z-[1]"
+        />
+      </div>
     </header>
   );
 };
