ca5914b517bc09c9cd547cd5beb5263472ef6f0a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-07-02 22:21:42 +0300

    Responsive navbar

--- src/app/components/layout/navbar/Navbar.tsx
+++ src/app/components/layout/navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import Search from "./Search";
 
 const Navbar = (): JSX.Element => {
   return (
-    <nav className="flex flex-wrap items-center justify-between px-1 py-7">
-      <div className="basis-[15%]">
+    <nav className="flex flex-col lg:flex-row items-center justify-between gap-[50px] lg:gap-0 px-1 py-7">
+      <div className="md:basis-[15%]">
         <Logo />
       </div>
       <Links />
